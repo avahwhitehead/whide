74cd14ea6d5621db02cffce7c49a56c8965740fd
sonrad10 <alaric.whitehead@gmail.com>
2020-12-13 22:43:27 +0000

    Added code editor. Started adding basic home page layout.

---
+++ src/components/CodeEditor.vue
@@ -0,0 +1,61 @@
+<template>
+	<!-- This div will hold the code editor -->
+	<div ref="codeHolder" class="codeHolder"></div>
+</template>
+
+
+<script>
+//The code editor
+import CodeMirror from "codemirror";
+//Directly import the CSS for the language syntax
+import 'codemirror/lib/codemirror.css';
+
+export default {
+	name: 'CodeEditor',
+	props: {
+		value: String
+	},
+	mounted() {
+		//Create the code editor in the div, using the provided options
+		let codeHolder = this.$refs.codeHolder;
+		//let codeHolder = this.$refs.textarea;
+		this._editor = CodeMirror(codeHolder, {
+		//this._editor = CodeMirror.fromTextArea(codeHolder, {
+			lineNumbers: true,
+			tabSize: 4,
+			value: this.code,
+		});
+		//Pass the change event (when the content changes at all) up to the next level
+		this._editor.on("change", () => {
+			this.$emit("change", this._editor?.getValue());
+		});
+	},
+	data() {
+		return {
+			//The actual code editor object.
+			editor: undefined,
+			//The string currently shown in the editor
+			code: this.$props.value || "",
+		}
+	},
+	methods: {
+		/**
+		 * Update the editor content when the <code>value</code> parameter updates
+		 */
+		onValueChange() {
+			if (!this._editor) return;
+			let cursor = this._editor.getCursor();
+			this.code = this.$props.value;
+			this._editor.setValue(this.code);
+			this._editor.setCursor(cursor);
+		}
+	}
+}
+</script>
+
+<style>
+.codeHolder {
+	text-align: left;
+	height: 100%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,99 @@
 <template>
 	<div class="home">
-		<img alt="Vue logo" src="../assets/logo.png">
-		<HelloWorld msg="Welcome to Your Vue.js App"/>
+		<div class="header">
+			<div class="filler">
+				Header content
+			</div>
+		</div>
+
+		<div class="left">
+			<div class="file-select">
+				<div class="filler">
+					Select file
+				</div>
+			</div>
+		</div>
+
+		<div class="code-editor">
+			<CodeEditor />
+		</div>
+
+		<div class="right">
+			<div class="filler">
+				Other content
+			</div>
+		</div>
+
+		<div class="footer">
+			<div class="filler">
+				Footer content
+			</div>
+		</div>
 	</div>
 </template>
 
+<style>
+.home {
+
+}
+
+.header, .footer {
+	width: 100%;
+}
+
+.header {
+
+}
+
+.footer {
+
+}
+
+
+.left, .right {
+	width: 15%
+}
+
+.left {
+	float: left;
+}
+
+.right {
+	float: right;
+}
+
+
+.code-editor {
+	outline: 1px solid #AAA;
+	height: 20em;
+	width: 70%;
+	display: inline-block;
+}
+
+/*
+Fillers
+*/
+
+.filler {
+	background: #999;
+	width: 100%;
+	height: 20em;
+}
+
+.header .filler, .footer .filler {
+	background: #CCC;
+	height: 3em;
+}
+</style>
+
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import CodeEditor from "@/components/CodeEditor";
 
 export default {
 	name: 'Home',
 	components: {
-		HelloWorld
+		CodeEditor,
 	}
 }
 </script>
