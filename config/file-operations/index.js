07f112063cdb5158ced4e909663c605573d5118e
sonrad10 <alaric.whitehead@gmail.com>
2021-01-27 15:13:10 +0000

    Implemented deleting files/folders as a plugin

--- config/file-operations/index.js
+++ config/file-operations/index.js
@@ -1,9 +1,11 @@
 import open from "./commands/open";
 import new_file from "./commands/new_file";
 import new_folder from "./commands/new_folder";
+import delete_file from "./commands/delete_file";
 
 export default [
 	open,
 	new_file,
 	new_folder,
+	delete_file,
 ];
