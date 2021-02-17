e4cc956f1d3177755c8bbedcd549bd2ba4bc55b1
Alaric Whitehead <alaric.whitehead@gmail.com>
2021-02-17 16:34:15 +0000

    feat: added automatic install of system plugin dependencies

---
+++ pluginInstall.js
@@ -0,0 +1,28 @@
+//Sourced from https://stackoverflow.com/a/31774097/2966288
+
+const fs = require('fs');
+const path = require('path');
+const cp = require('child_process');
+const os = require('os');
+
+// get library path
+const lib = path.resolve(__dirname, './config/');
+
+fs.readdirSync(lib).forEach(function(mod) {
+	const modPath = path.join(lib, mod);
+
+	// ensure path has package.json
+	if (!fs.existsSync(path.join(modPath, 'package.json'))) {
+		return;
+	}
+
+	// npm binary based on OS
+	const npmCmd = os.platform().startsWith('win') ? 'npm.cmd' : 'npm';
+
+	// install folder
+	cp.spawn(npmCmd, ['i'], {
+		env: process.env,
+		cwd: modPath,
+		stdio: 'inherit'
+	});
+})
