d84b87169391c3a8f262dcbf86ffd2f2a5bdf699
Alaric Whitehead <alaric.whitehead@gmail.com>
2021-02-18 13:38:21 +0000

    feat: added support for running code in the editor through the console

---
+++ config/hwhile-run/commands/run.js
@@ -0,0 +1,46 @@
+import tmp from "tmp";
+import fs  from "fs";
+import path from "path";
+import { HWhileConnector } from "hwhile-wrapper";
+
+export const name = "run_code";
+export const args = [];
+
+export async function run({ editorController, ioController }) {
+	const code = await editorController.editor.getValue();
+
+	let name = await ioController.getInput({
+		message: "File name to use",
+		title: "File name",
+	});
+	if (!name) {
+		await ioController.showOutput({title:"Cancelled Run"});
+		return;
+	}
+	let expr = await ioController.getInput({
+		message: "Expression to pass as input to the program",
+		title: "Argument expression",
+	});
+	if (!expr) {
+		await ioController.showOutput({title:"Cancelled Run"});
+		return;
+	}
+
+	const tmp_folder = tmp.dirSync();
+	const folder_path = tmp_folder.name;
+	console.log('Dir: ', folder_path);
+
+	fs.writeFileSync(path.join(folder_path, name), code);
+
+	const hWhileConnector = new HWhileConnector({
+		hwhile: "hwhile",
+		cwd: folder_path,
+	});
+
+	let shell = hWhileConnector.run(name, expr, false);
+
+	shell.stdout.on("data", data => console.log(data.toString()));
+	shell.stderr.on("data", data => console.log(`stderr: ${data}`));
+	shell.on('error', error => console.log(` error: ${error.message}`));
+	shell.on("close", code => console.log(`child process exited with code ${code}`));
+}
---
+++ config/hwhile-run/index.js
@@ -0,0 +1,15 @@
+import * as run from "./commands/run.js";
+
+export const menus = [
+	{
+		"name": "Run",
+		"children": [
+			{ "name": "Run", "command": "run_code" },
+			{ "name": "Debug", "command": "run_code" }
+		]
+	}
+];
+
+export default [
+	run
+];
