6deb39a76a54d0c3d107ff7d3db68533b3dcd44f
Alaric Whitehead <alaric.whitehead@gmail.com>
2021-03-04 20:30:48 +0000

    fix: fixed delete option not working

--- config/file-operations/commands/delete_file.js
+++ config/file-operations/commands/delete_file.js
@@ -11,9 +11,23 @@ module.exports.args = [
 
 module.exports.run = async function ({args, ioController, fs}) {
 	const filePath = args["path"];
-	//Delete the file (or folder)
-	fs.rm(filePath, { recursive: true }, err => {
-		if (err) _displayError(ioController, err);
-		else console.log(`Successfully created file "${filePath}"`);
+	//See if the path is a file or a folder
+	fs.stat(filePath, (err, stat) => {
+		if (err) {
+			if (err) _displayError(ioController, err);
+			return;
+		}
+		//Callback when deleting completes
+		const callback = (e) => {
+			if (e) _displayError(ioController, e);
+			else console.log(`Successfully deleted "${filePath}"`);
+		};
+
+		//Delete the file or folder at the path
+		if (stat.isDirectory()) {
+			fs.rmdir(filePath, callback);
+		} else {
+			fs.unlink(filePath, callback);
+		}
 	});
 }
