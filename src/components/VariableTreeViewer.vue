2699de03cc3c993ec0dcf9808a279f740bed508b
Alaric Whitehead <alaric.whitehead@gmail.com>
2021-04-22 18:05:43 +0100

    fix: fixed tree viewer not scaling properly for large trees feat: added a "reset view" button to the tree viewer

--- src/components/VariableTreeViewer.vue
+++ src/components/VariableTreeViewer.vue
@@ -2,18 +2,17 @@
 	<div class="parent" @scroll.prevent="">
 		<svg
 			:height="height"
-			:viewBox="viewbox"
 			preserveAspectRatio="xMinYMin meet"
-			@mousedown="startDrag"
-			@mouseup="stopDrag"
-			@mousemove="handleDrag"
-			@wheel="handleZoom"
+			ref="svg-el"
 		>
-			<g ref="mysvg"/>
+			<g ref="svg-root">
+				<NodeGroup :d="d" v-for="(d, i) in nodes" :key="i" />
+			</g>
 		</svg>
 		<div class="controls">
-			<input type="button" class="button" @click="() => _zoom(1)" value="+">
-			<input type="button" class="button" @click="() => _zoom(-1)" value="-">
+			<input type="button" class="button" @click="() => _zoom(1)" value="+" />
+			<input type="button" class="button" @click="() => _zoom(-1)" value="-" />
+			<input type="button" class="button" @click="focus" value="Reset" />
 		</div>
 	</div>
 </template>
@@ -21,7 +20,7 @@
 <script lang="ts">
 import Vue from "vue";
 import * as d3 from "d3";
-import { HierarchyPointNode } from "d3";
+import { HierarchyPointNode, TreeLayout, ZoomBehavior, ZoomedElementBaseType } from "d3";
 import NodeGroup from "@/components/_internal/trees/NodeGroup.vue";
 
 export interface TreeType {
@@ -41,28 +40,17 @@ interface DataTypeInterface {
 	},
 	width: number;
 	height: number;
-	//Information for dragging the svg canvas
-	drag: {
-		//Whether the canvas is being dragged
-		dragging: boolean;
-		//Current X/Y offset
-		offset: {
-			x: number;
-			y: number;
-		};
-		//The X/Y offset when the dragging started
-		start: {
-			x: number;
-			y: number;
-		};
-	};
-	scale: number;
+	zoom?: ZoomBehavior<ZoomedElementBaseType, unknown>;
+	nodes?: HierarchyPointNode<unknown>;
 }
 
 export default Vue.extend({
 	name: 'VariableTreeViewer',
 	props: {
 		tree: Object as () => TreeType,
+	},
+	components: {
+		NodeGroup,
 	},
 	data() : DataTypeInterface {
 		return {
@@ -74,64 +62,44 @@ export default Vue.extend({
 			},
 			width: 660,
 			height: 500,
-			drag: {
-				dragging: false,
-				offset: { x: -66, y: 50 },
-				start: { x: 0, y: 0 },
-			},
-			scale: 1.25
+			zoom: undefined,
+			nodes: undefined,
 		};
 	},
 	computed: {
 		diagramWidth() : number {
-			return this.width - this.margin.left - this.margin.right;
+			let scale = 1;//(this.nodes?.height || 10) / 1;
+			return (this.width - this.margin.left - this.margin.right) * scale;
 		},
 		diagramHeight() : number {
-			return this.height - this.margin.top - this.margin.bottom;
-		},
-		viewbox() : string {
-			const X = this.drag.offset.x;
-			const Y = this.drag.offset.y;
-			const width = this.width;
-			const height = this.height;
-			const scale = this.scale;
-			return `${-X / scale} ${-Y / scale} ${width / scale} ${height / scale}`;
+			let scale = 1;//(this.nodes?.height || 10) / 1;
+			return (this.height - this.margin.top - this.margin.bottom) * scale;
 		},
 	},
 	methods: {
-		startDrag(event: MouseEvent) {
-			this.drag.dragging = true;
-			//Save the cursor position at the start of the drag
-			this.drag.start = {
-				x: event.offsetX - this.drag.offset.x,
-				y: event.offsetY - this.drag.offset.y,
-			};
-		},
-		stopDrag() {
-			this.drag.dragging = false;
-		},
-		handleDrag(event: MouseEvent) {
-			if (!this.drag.dragging) return;
-			//Update the offset to be the difference in position since the `startDrag` was called
-			this.drag.offset = {
-				x: event.offsetX - this.drag.start.x,
-				y: event.offsetY - this.drag.start.y,
-			}
-		},
-		handleZoom(event: WheelEvent) {
-			//Zoom in the scroll direction
-			this._zoom(-event.deltaY);
-			//Don't scroll the page
-			event.preventDefault();
-		},
+		/**
+		 * Zoom the diagram
+		 * @param direction     Positive value to zoom in, negative value to zoom out.
+		 */
 		_zoom(direction: number): void {
-			//Fixed jump size on all browsers
-			let scale = this.scale + .2 * (direction >= 0 ? 1 : -1);
-			//Limit zooming in/out
-			scale = Math.max(scale, 0.3);
-			scale = Math.min(scale, 5);
-			//Save the new scale
-			this.scale = scale;
+			const svg = d3.select(this.$refs["svg-el"] as Element);
+			if (!this.zoom) return;
+			svg.transition().duration(200).call(
+				this.zoom.scaleBy,
+				(direction >= 0) ? 1.25 : .75
+			);
+		},
+		/**
+		 * Reset the diagram focus to its initial scale/location
+		 */
+		focus() {
+			if (!this.zoom) return;
+			const svg = d3.select(this.$refs["svg-el"] as Element);
+			svg.transition().duration(200).call(
+				this.zoom.transform,
+				d3.zoomIdentity,
+				d3.zoomTransform(svg.node() as any).invert([this.diagramWidth / 2, this.diagramHeight / 2])
+			);
 		},
 
 		/**
@@ -139,31 +107,40 @@ export default Vue.extend({
 		 * Based on this example: https://bl.ocks.org/d3noob/72f43406bbe9e104e957f44713b8413c
 		 * @param treeData
 		 */
-		drawTree(treeData: TreeType) {
-			//Tell d3 to work with a tree, and stay in the diagram limits
-			let treemap = d3.tree().size([this.diagramWidth, this.diagramHeight]);
+		drawTree(treeData: TreeType): void {
+			//Build a d3 tree from the nodes
+			let nodes = d3.hierarchy(treeData);
 
-			//Use the parent-child hierarchy to build the tree
-			let nodes: HierarchyPointNode<unknown> = treemap(d3.hierarchy(treeData));
+			//Scale for the diagram based on the tree size
+			let scale = Math.max((nodes?.height || 10), 10);
 
-			//Get the SVG element, and remove any existing child nodes
-			const svg = d3.select(this.$refs["mysvg"] as Element);
-			svg.selectChildren().remove();
-
-			//SVG root group element
-			let root = svg.append("g");
-
-			//For each node in the tree...
-			root.selectAll(".node").data(nodes.descendants()).enter()
-				//Add a NodeGroup element
-				.append((d: any) => new NodeGroup({propsData: {d}}).$mount().$el);
+			//Draw the tree, dynamically setting the sizes
+			let treemap: TreeLayout<unknown> = d3.tree().size([scale * 100, scale * 100]);
+			this.nodes = treemap(d3.hierarchy(treeData));
 		}
 	},
 	mounted() {
+		//Get the SVG elements
+		const svg = d3.select(this.$refs["svg-el"] as Element);
+		const parentG = d3.select(this.$refs["svg-root"] as Element);
+
+		//Set up panning on the SVG element
+		svg.attr("viewBox", `${0}, ${0}, ${this.diagramWidth}, ${this.diagramHeight}`);
+		//Set up zooming on the SVG element
+		this.zoom = d3.zoom();
+		svg.call(this.zoom
+			.extent([[0, 0], [this.diagramWidth, this.diagramHeight]])
+			.scaleExtent([.1, 10])
+			.on("zoom", ({ transform } : { transform: { k:number, x:number, y:number } }) => {
+				parentG.attr("transform", transform as any);
+			})
+		);
+		//Draw the initial tree
 		this.drawTree(this.tree);
 	},
 	watch: {
 		tree(newTree: TreeType): void {
+			//Draw the new tree
 			this.drawTree(newTree);
 		}
 	}
