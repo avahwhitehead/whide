1da00d2632498dc485deac15d1458fb466bb39fb
sonrad10 <alaric.whitehead@gmail.com>
2021-09-01 12:04:49 +0100

    fix: stopped error messages being shown as intrusive alerts

--- src/views/App.vue
+++ src/views/App.vue
@@ -3,22 +3,13 @@
 		<v-main>
 			<router-view></router-view>
 		</v-main>
-
-		<InputPrompt @controller="ioControllerChange" />
 	</v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import InputPrompt from "@/components/InputPrompt.vue";
-import { IOController } from "@/types";
 import { APP_THEME } from "@/plugins/vuex";
 import '@fortawesome/fontawesome-free/css/all.css';
-
-//TODO: Use v-alert for displaying errors
-//  https://vuetifyjs.com/en/components/alerts/#prominent
-
-let ioController: IOController|undefined;
 
 //Check whether the user's browser explicitly requests light or dark theme
 let darkThemeCheckQuery = window.matchMedia('(prefers-color-scheme: dark)');
@@ -26,13 +17,7 @@ let lightThemeCheckQuery = window.matchMedia('(prefers-color-scheme: light)');
 
 export default Vue.extend({
 	name: 'app',
-	components: {
-		InputPrompt
-	},
 	methods: {
-		ioControllerChange(controller?: IOController) {
-			ioController = controller;
-		},
 		updateTheme(theme: APP_THEME): void {
 			if (theme === APP_THEME.AUTO) {
 				//Listen for updates in requested theme
@@ -77,34 +62,6 @@ export default Vue.extend({
 		}
 	}
 });
-
-Vue.config.errorHandler = function (err, _, info) {
-	let msg = `${err.toString()}\nInfo: ${info}`;
-	//Log the error
-	console.error(msg);
-	//Show in a popup
-	ioController?.prompt({
-		title: `Uncaught Error: ${err.name}`,
-		message: msg,
-		options: ['Ok']
-	});
-}
-
-// window.onerror = function (msg:Event|string, url?:string, line?:number, col?:number, err?:Error) {
-// 	//https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers/onerror
-// 	let message: string = '';
-// 	if (err) message = err.toString() + '\n';
-// 	message += `Info: ${msg}`;
-//
-// 	//Log the error
-// 	console.error(message);
-// 	//Show in a popup
-// 	ioController?.prompt({
-// 		title: `Uncaught Error: ${err ? err.name : 'Unknown Error'}`,
-// 		message: message,
-// 		options: ['Ok']
-// 	});
-// }
 </script>
 
 <!--suppress CssUnusedSymbol -->
