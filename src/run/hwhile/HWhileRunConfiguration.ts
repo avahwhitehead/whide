7c916629a967fa4c2e6827170f93825a0c529f55
sonrad10 <alaric.whitehead@gmail.com>
2021-11-21 13:44:04 +0000

    feat: updated HWhile wrapper to reduce the amount of unnecessary information in the debugger output

--- src/run/hwhile/HWhileRunConfiguration.ts
+++ src/run/hwhile/HWhileRunConfiguration.ts
@@ -3,7 +3,6 @@ import { BaseDebugger, BaseRunner } from "@/run/BaseRunner";
 import path from "path";
 import { HWhileConnector, InteractiveHWhileConnector, RunResultType, StepResultType } from "@whide/hwhile-wrapper";
 import { BinaryTree } from "whilejs";
-import { treeParser } from "@whide/tree-lang";
 import { stringifyTree } from "@/utils/tree_converters";
 import { ChildProcessWithoutNullStreams } from "child_process";
 
@@ -112,7 +111,6 @@ export class HWhileRunner extends BaseRunner {
 export class HWhileDebugger extends BaseDebugger {
 	private _props: HWhileDebugConfigurationProps;
 	private hWhileConnector: InteractiveHWhileConnector|undefined;
-	private _currentState: ProgramState|undefined;
 	private _progName: string|undefined;
 	private _allowRun: boolean;
 	private _allowStep: boolean;
@@ -147,7 +145,7 @@ export class HWhileDebugger extends BaseDebugger {
 		//Start the interpreter
 		await this.hWhileConnector.start();
 		//Load the chosen program
-		await this.hWhileConnector.load(this._progName, this._props.expression, true);
+		await this.hWhileConnector.load(this._progName, this._props.expression, true, false);
 
 		//Setup the program breakpoints
 		for (let b of this._props.breakpoints || []) {
@@ -161,13 +159,13 @@ export class HWhileDebugger extends BaseDebugger {
 	async run(): Promise<ProgramState> {
 		//Run the program
 		let result = await this.hWhileConnector!.run(true);
-		return await this._afterRun(result);
+		return this._afterRun(result);
 	}
 
 	async step(): Promise<ProgramState> {
 		//Step over the next line in the program
 		let result = await this.hWhileConnector!.step(true);
-		return await this._afterRun(result);
+		return this._afterRun(result);
 	}
 
 	/**
@@ -175,24 +173,19 @@ export class HWhileDebugger extends BaseDebugger {
 	 * @param result	The result of the run/step operation
 	 * @private
 	 */
-	private async _afterRun(result: StepResultType|RunResultType) {
+	private async _afterRun(result: StepResultType|RunResultType): Promise<ProgramState> {
 		//Read the updated variable values
-		this._variables = await this.hWhileConnector!.store(true);
-		//Stop the process here if the program is done
-		if (result.cause === 'done') {
+		this._variables = result.allVariables;
+		if (result.done) {
+			this._allowRun = false;
+			this._allowStep = false;
 			await this.stop();
-			this._currentState = {
-				variables: this._variables,
-				done: true,
-			};
-		} else {
-			this._currentState = {
-				variables: this._variables,
-				done: false,
-			};
 		}
-		//Return the program state
-		return this._currentState;
+		return {
+			done: result.done,
+			variables: result.allVariables,
+			currentLine: result.line
+		};
 	}
 
 	async stop(): Promise<void> {
@@ -203,42 +196,21 @@ export class HWhileDebugger extends BaseDebugger {
 		this._allowRun = false;
 	}
 
-	async set(name: string, value: BinaryTree|string, program?: string): Promise<ProgramState> {
-		if (!program) program = this._progName!;
-
+	async set(name: string, value: BinaryTree|string): Promise<ProgramState> {
 		//Ensure there is a version of the tree as a string and a BinaryTree
-		let tree: BinaryTree;
 		let treeString: string;
 		if (typeof value === 'string') {
 			treeString = value;
-			tree = treeParser(value);
 		} else {
-			tree = value;
 			treeString = stringifyTree(value);
 		}
 
-		//Update the tree in HWhile
-		await this.hWhileConnector!.execute(`${name} := ${treeString}`, true);
-
-		//Create the program state if necessary
-		if (!this._currentState) this._currentState = {};
-		if (!this._currentState.variables) {
-			//Fetch the variable values from the interpreter
-			this._currentState.variables = await this.hWhileConnector!.store(true);
-		}
-
-		//Update the tree value in the program state
-		let programMap: Map<string,BinaryTree>|undefined = this._currentState.variables.get(program);
-		if (programMap) {
-			programMap.set(name, tree);
-		} else {
-			programMap = new Map();
-			programMap.set(name, tree);
-			this._currentState.variables.set(program, programMap);
-		}
-
-		//Return the program state
-		return this._currentState;
+		let res = await this.hWhileConnector!.setVariable(name, treeString);
+		return {
+			done: res.done,
+			variables: res.allVariables,
+			currentLine: res.line,
+		};
 	}
 
 	get variables(): Map<string, Map<string, BinaryTree>> {
